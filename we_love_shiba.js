195e59d488f646e3b213b5db026c2ad83e10093b
Clément AZIBEIRO <clement.azibeiro@bts-malraux.net>
2020-02-17 21:33:23 +0100

    Commencement

---
+++ we_love_shiba.js
@@ -0,0 +1,7 @@
+const config = require('./config.json');
+
+const api_key = config.api_key;
+const api_secret_key = config.api_secret_key;
+const access_token = config.access_token;
+const access_token_secret = config.access_token_secret;
+
