04496158c45101bdc5f5ee029bdff46855780599
Clément AZIBEIRO <clement.azibeiro@bts-malraux.net>
2020-02-17 22:48:48 +0100

    change T

--- we_love_shiba.js
+++ we_love_shiba.js
@@ -3,6 +3,6 @@ var config = require('./config.js');
 
 var twitter = new twit(config);
 
-T.get('search/tweets', { q: 'shiba since:2011-07-11', count: 100 }, function(err, data, response) {
+twitter.get('search/tweets', { q: 'shiba since:2011-07-11', count: 100 }, function(err, data, response) {
     console.log(data)
   })
