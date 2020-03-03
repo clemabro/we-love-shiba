2103d9f7637ecc503ba8a16c62158f01bf5e9bb5
clemabro <46746462+clemabro@users.noreply.github.com>
2020-03-03 09:32:41 +0100

    Update we_love_shiba.js

--- we_love_shiba.js
+++ we_love_shiba.js
@@ -32,7 +32,7 @@ function start() {
     });
   });
   
-  var h = schedule.scheduleJob('30 9 * * *', function(){
+  var h = schedule.scheduleJob('* 35 9 * * *', function(){
     giphy.random(randomGif).then(function (res){
       console.log(res.data.image_mp4_url);
       postImg(res.data.image_mp4_url);
