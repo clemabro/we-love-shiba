27899af2e54828a31a6753333a7c045e814cbab9
clemabro <46746462+clemabro@users.noreply.github.com>
2020-03-05 09:28:32 +0100

    Create downloadImg.js

---
+++ downloadImg.js
@@ -0,0 +1,45 @@
+var twit = require('twit');
+var config = require('./config.js');
+var schedule = require('node-schedule');
+var throng = require('throng');
+var giphy = require('giphy-api')('doyfDKYfROgpVGx7qdWwc0A4h8rCAFo3');
+var fs = require('fs');
+const Path = require('path');
+const Listr = require('listr');
+const Axios = require('axios');
+
+giphy.random(randomGif).then(function (res){
+  console.log("Téléchargement de cette video : " + res.data.image_mp4_url);
+  downloadImg(res.data.image_mp4_url);
+})
+
+function downloadImg(img_url) {
+  var tasks = [{
+    title: 'Downloading',
+    task: async (ctx, task) => {
+      const url = img_url;
+      const path = Path.resolve(__dirname, 'images', 'imgpost.mp4')
+
+      const response = await Axios({
+        method: 'GET',
+        url: url,
+        responseType: 'stream'
+      })
+
+      response.data.pipe(fs.createWriteStream(path))
+
+      return new Promise((resolve, reject) => {
+        response.data.on('end', () => {
+          resolve()
+        })
+
+        response.data.on('error', err => {
+          reject(err)
+        })
+      })
+    }
+  }]
+
+
+  new Listr(tasks).run().then(tasks = null);
+};
